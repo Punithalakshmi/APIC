07987bcfad5e40d2d7a55dc40b080efc6992d22a
Punitha <punitha.lakshmi87@gmail.com>
2024-12-27 14:04:11 +0530

    APIC Delete account api integrated

--- resources/views/admin/dealer/list.blade.php
+++ resources/views/admin/dealer/list.blade.php
@@ -96,7 +96,9 @@
                                     <div class="btn-group" role="group" aria-label="Basic example">
                                         <a href="{{ route('admin/dealers/edit', ['id'=>$dealer->id]) }}" type="button" class="btn btn-secondary">Edit</a>
                                         <a href="{{ route('admin/dealers/delete', ['id'=>$dealer->id]) }}" onclick="return confirm('Are you sure you want to delete this dealer?');" type="button" class="btn btn-danger">Delete</a>
-                                        <a href="{{ route('admin/dealers/upgrade', ['id'=>$dealer->id]) }}" onclick="return confirm('Are you sure you want to upgrade this dealer?');" type="button" class="btn btn-primary">Upgrade</a>
+                                        @if($dealer->apic_user_type != 'Pro')
+                                          <a href="{{ route('admin/dealers/upgrade', ['id'=>$dealer->id]) }}" onclick="return confirm('Are you sure you want to upgrade this dealer?');" type="button" class="btn btn-primary">Upgrade</a>
+                                        @endif 
                                           @if($dealer->token == '') 
                                            <a href="{{ route('admin/api/register', ['id'=>$dealer->id]) }}" type="button" class="btn btn-primary">Register</a>
                                           @endif  
