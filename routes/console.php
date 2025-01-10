fa85cb89d6578d711d12169079563f7c452efd98
Punithalakshmi <punitha.lakshmi87@gmail.com>
2025-01-10 06:56:21 +0000

    Test

--- routes/console.php
+++ routes/console.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote')->everyMinute();
 
 
 //Schedule::command('coohom:cron')->everyMinute();
 
-Schedule::command('app:test-job')->everyFiveMinutes();
+Schedule::command('app:test-job')->everyMinute();
 
